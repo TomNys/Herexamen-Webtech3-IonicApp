0b4b198f5ed5a26fff2613690e0e488ea85a47b4
Tom Nys <tom.nys@student.ap.be>
2015-09-01 16:47:34 +0200

    Controller changes
    Veranderingen en afwerken controller

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,2 +1,36 @@
 angular.module('starter.controller', [])
 
+.controller('indexController', function ($scope, $ionicModal, $window, $log, ballService, $rootScope) {
+    $scope.answers = ballService.getAllAnswers();
+    $scope.askedQuestions = [];
+	$scope.usedAnswers = [];
+	$scope.askedQuestionTracker = 0;
+	$answer = "";
+	$scope.askedQuestions.answers = [];
+	
+	$scope.addQuestion = function(newQuestion) {
+		
+        if (newQuestion != undefined) {
+			if( $scope.askedQuestions.indexOf(newQuestion) == -1){
+				$scope.askedQuestions.push(newQuestion);
+				$scope.answer = $scope.answers[Math.floor(Math.random()*20)];
+				if ($scope.askedQuestionTracker <= 19){
+					while($scope.usedAnswers.indexOf($scope.answer) != -1){
+						$scope.answer = $scope.answers[Math.floor(Math.random()*20)];
+					}
+				}
+				$scope.usedAnswers.push($scope.answer);
+				$scope.askedQuestions.answers[$scope.askedQuestions.indexOf(newQuestion)] = $scope.answer;
+				$window.alert($scope.answer);
+				//$window.alert($scope.askedQuestions.indexOf(newQuestion));
+				//$window.alert($scope.askedQuestions.answers[$scope.askedQuestions.indexOf(newQuestion)]);
+				$scope.askedQuestionTracker += 1;
+			
+			}
+			else $window.alert($scope.askedQuestions.answers[$scope.askedQuestions.indexOf(newQuestion)]);
+        }
+       
+    };
+	
+    
+})
